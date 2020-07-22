651b9e7449465adb0ede0a3560d0d9f9ebcbc0dd
Fatima <anne.wi.sw@gmail.com>
2020-07-22 23:45:17 +0300

    make it better7

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -154,3 +156,5 @@ EMAIL_HOST_PASSWORD = 'veer.zaara'
 EMAIL_PORT = 587
 
 DJANGO_NOTIFICATIONS_CONFIG = { 'SOFT_DELETE': True}
+
+django_heroku.settings(locals())
